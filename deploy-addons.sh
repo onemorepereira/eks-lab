5e624681cfba809d1ba027cbeec808498d88fd2a
Miguel Pereira <miguel@myrotarycar.com>
2019-11-05 18:58:31 -0600

    Add ArgoCD as an add-on to the cluster

--- deploy-addons.sh
+++ deploy-addons.sh
@@ -2,7 +2,13 @@
 
 for AO in $(find ./add-ons/ |sort ); 
   do
-    [[ "$AO" == *".yaml" ]] && \
-    echo  "#### Deploying Add On: ${AO} ####" &&  \
-    kubectl apply -f ${AO};
+    if [[ "$AO" == *".yaml" ]]; then
+      echo  "#### Deploying Add On: ${AO} ####";
+      # ArgoCD is special
+      if [[ "$AO" == *"argo-custom.yaml" ]]; then
+        kubectl apply -n argocd -f ${AO};
+      else
+        kubectl apply -f ${AO};
+      fi
+    fi
   done
